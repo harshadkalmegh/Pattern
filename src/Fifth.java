3ea3e4b3c0cea6232d02be193bbb52c8625ade61
Harshad B. Kalmegh <hkalmegh@dc2.propsol.org>
2018-09-20 16:56:13 +0530

    Pattern

---
+++ src/Fifth.java
@@ -0,0 +1,25 @@
+
+public class Fifth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			for (int i=5; i>=1; i--){
+			
+			for (int j=5; j>i; j--){
+				
+				System.out.print(' ');
+			}
+			
+			for (int k=0;k<i;k++){
+				
+				System.out.print("*");
+			
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
