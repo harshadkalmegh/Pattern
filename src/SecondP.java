7b09f80e12589e5a497d9080292abc1afca60b4f
Harshad B. Kalmegh <hkalmegh@dc2.propsol.org>
2018-09-20 16:50:58 +0530

    Pattern

---
+++ src/SecondP.java
@@ -0,0 +1,26 @@
+
+public class SecondP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i=1; i<5; i++){
+			
+			for (int j=3; j>=i; j--){
+				
+				System.out.print(' ');
+			}
+			
+			for (int k=0;k<i;k++){
+				
+				System.out.print("*");
+			
+			}
+			
+			System.out.println();
+		}
+	}
+}
+
+
+
